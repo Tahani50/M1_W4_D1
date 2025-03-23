93fed742ecddf4dc8cc470b692b562596aa0cb04
Tahani50 <tahanyayman4@gmail.com>
2025-03-23 12:47:35 +0300

    Initial Commit

---
+++ M1_W4_D1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  M1_W4_D1
+//
+//  Created by Taibah Valley Academy on 3/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ M1_W4_D1/M1_W4_D1App.swift
@@ -0,0 +1,17 @@
+//
+//  M1_W4_D1App.swift
+//  M1_W4_D1
+//
+//  Created by Taibah Valley Academy on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct M1_W4_D1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
